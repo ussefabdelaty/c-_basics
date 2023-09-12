b07f3de9c85f413e6623329314b025dc9766a93f
unknown <ussifyounis@gmail.com>
2023-09-13 02:29:11 +0300

    fizz_buzz is a simple programming task the instructions is For multiples of 3, print Fizz instead of the number. For multiples of 5, print Buzz instead of the number.For numbers that are multiples of both 3 and 5, print FizzBuzz.

---
+++ fizz_buzz/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace fizz_buzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzBuzz");
+                }
+                else if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else { Console.WriteLine(i); }
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
